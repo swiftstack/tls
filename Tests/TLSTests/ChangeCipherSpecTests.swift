cce61acaca804dedfdfb466c92a8b00fe012ca06
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-12 17:52:54 +0300

    [Tests] ChangeCipherSpec

---
+++ Tests/TLSTests/ChangeCipherSpecTests.swift
@@ -0,0 +1,26 @@
+import Test
+import Stream
+@testable import TLS
+
+class ChangeCipherSpecTests: TestCase {
+    @nonobjc
+    let bytes: [UInt8] = [0x14, 0x03, 0x03, 0x00, 0x01, 0x01]
+
+    func testDecode() {
+        scope {
+            let stream = InputByteStream(bytes)
+            let record = try RecordLayer(from: stream)
+            assertEqual(record.version, .tls12)
+            assertEqual(record.content, .changeChiperSpec(.default))
+        }
+    }
+
+    func testEncode() {
+        scope {
+            let stream = OutputByteStream()
+            let record = RecordLayer(version: .tls12, content: .changeChiperSpec(.default))
+            try record.encode(to: stream)
+            assertEqual(stream.bytes, bytes)
+        }
+    }
+}
