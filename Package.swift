4013d1aba9bfde24292a07871aec3d61d3d9faf5
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-20 08:31:53 +0300

    [Package] Bump darwin platforms

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "TLS",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
+        .iOS(.v16),
+        .macOS(.v13),
     ],
     products: [
         .library(
